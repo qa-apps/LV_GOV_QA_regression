575e0cc66dcbd8a1b43ef9af6cc1643cbcc13964
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-01-11 11:36:20 -0500

    tests: add 5 cases (batch 1)

--- tests/test_home_a11y.py
+++ tests/test_home_a11y.py
@@ -60,3 +60,38 @@ def _meta_commit_1():
 
 def _meta_commit_11():
     return 11
+
+@pytest.mark.ui
+def test_auto_1_a(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.logo).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_1_b(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.search_input).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_1_c(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.login_link).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_1_d(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.services_menu).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_1_e(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.all_services_link).first).to_be_visible()
