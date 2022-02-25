acc92feaed410f8760d4fdbd5bd6f12935512a5a
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-02-25 12:10:00 -0500

    Add more navigation menu tests
    - test_my_latvija_menu_opens
    - test_e_address_menu_opens

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -19,4 +19,20 @@ class TestNavigation:
         
         page.locator('button:has-text("Ko darīt, ja...?")').first.click()
         page.wait_for_timeout(500)
+    
+    @pytest.mark.regression
+    def test_my_latvija_menu_opens(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        page.locator('button:has-text("Mana Latvija.lv")').first.click()
+        page.wait_for_timeout(500)
+    
+    @pytest.mark.regression
+    def test_e_address_menu_opens(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        page.locator('button:has-text("E-adrese")').first.click()
+        page.wait_for_timeout(500)
 
