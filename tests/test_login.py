c34a1959a7f59aaf527e8cd1d1e4f42c26713e5e
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-03-02 10:31:00 -0500

    Add login functionality tests
    - test_login_link_visible
    - test_login_link_has_href

---
+++ tests/test_login.py
@@ -0,0 +1,23 @@
+import pytest
+from playwright.sync_api import Page
+from pages.home_page import HomePage
+
+class TestLogin:
+    
+    @pytest.mark.regression
+    def test_login_link_visible(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        login_link = page.locator('a:has-text("Ienākt Mana Latvija.lv")').first
+        assert login_link.is_visible()
+    
+    @pytest.mark.regression
+    def test_login_link_has_href(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        login_link = page.locator('a:has-text("Ienākt Mana Latvija.lv")').first
+        href = login_link.get_attribute('href')
+        assert href is not None
+
