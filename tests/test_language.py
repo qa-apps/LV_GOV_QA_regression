20c9daa19335ef4cd3252bcc2b4c95a0becd31ab
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-03-05 19:00:00 -0500

    Add more language tests
    - test_language_switcher_has_href
    - test_current_language_displayed

--- tests/test_language.py
+++ tests/test_language.py
@@ -19,4 +19,21 @@ class TestLanguage:
         
         lang_switcher = page.locator('a[aria-label="Pārslēgties uz angļu valodu"]').first
         assert lang_switcher.is_enabled()
+    
+    @pytest.mark.regression
+    def test_language_switcher_has_href(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        lang_switcher = page.locator('a[aria-label="Pārslēgties uz angļu valodu"]').first
+        href = lang_switcher.get_attribute('href')
+        assert href is not None
+    
+    @pytest.mark.regression
+    def test_current_language_displayed(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        page_content = page.content()
+        assert "LV" in page_content or "Latvian" in page_content.lower()
 
