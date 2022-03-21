ef5a2d0a03bc51ecef83701e6bb86ad2a31ed6ef
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-03-20 20:27:00 -0400

    Add more UI elements tests
    - test_main_content_visible
    - test_navigation_bar_present

--- tests/test_ui_elements.py
+++ tests/test_ui_elements.py
@@ -19,4 +19,20 @@ class TestUIElements:
         
         header = page.locator('header').first
         assert header.is_visible()
+    
+    @pytest.mark.regression
+    def test_main_content_visible(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        main_content = page.locator('main').first
+        assert main_content.is_visible()
+    
+    @pytest.mark.regression
+    def test_navigation_bar_present(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        nav = page.locator('nav').first
+        assert nav.is_visible()
 
