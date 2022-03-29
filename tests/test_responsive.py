f02e0bca52114fa7f1f5dffee94835f486c217c2
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-03-28 20:22:00 -0400

    Add more responsive tests
    - test_viewport_height_correct
    - test_layout_not_broken

--- tests/test_responsive.py
+++ tests/test_responsive.py
@@ -18,4 +18,20 @@ class TestResponsive:
         home_page.open(base_url)
         
         assert page.viewport_size['width'] >= 1024
+    
+    @pytest.mark.regression
+    def test_viewport_height_correct(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        viewport = page.viewport_size
+        assert viewport['height'] == 1080
+    
+    @pytest.mark.regression
+    def test_layout_not_broken(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        header = page.locator('header').first
+        assert header.is_visible()
 
