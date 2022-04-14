1ba7182274572a4863a9ca2f7ae86870962c25e3
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-04-14 12:59:00 -0400

    Add more SEO tests
    - test_canonical_url
    - test_page_language_set

--- tests/test_seo.py
+++ tests/test_seo.py
@@ -19,4 +19,20 @@ class TestSEO:
         
         charset_meta = page.locator('meta[charset]')
         assert charset_meta.count() >= 0
+    
+    @pytest.mark.regression
+    def test_canonical_url(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        assert "latvija.gov.lv" in page.url
+    
+    @pytest.mark.regression
+    def test_page_language_set(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        html = page.locator('html').first
+        lang = html.get_attribute('lang')
+        assert lang is not None or "lv" in page.content().lower()
 
