9b0d107733115ae92ff328809132510f9e8d2cb8
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-05-25 16:00:00 -0400

    Add more security tests
    - test_secure_cookies
    - test_no_javascript_errors

--- tests/test_security.py
+++ tests/test_security.py
@@ -17,4 +17,18 @@ class TestSecurity:
         home_page.open(base_url)
         
         assert "https://" in page.url
+    
+    @pytest.mark.regression
+    def test_secure_cookies(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        assert page.url.startswith("https://")
+    
+    @pytest.mark.regression
+    def test_no_javascript_errors(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        assert home_page.is_logo_visible()
 
