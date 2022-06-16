a7f11ca1d33826024a0d80e92d9cd3f8f744ea8f
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-06-16 10:01:00 -0400

    Extend search tests
    - test_search_field_max_length
    - test_search_special_characters

--- tests/test_search.py
+++ tests/test_search.py
@@ -40,4 +40,23 @@ class TestSearch:
         search_input.fill("")
         search_input.fill("test2")
         assert "test2" in search_input.input_value()
+    
+    @pytest.mark.regression
+    def test_search_field_max_length(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        search_input = page.locator('input[aria-label*="Meklēšanas ievadlauks"]').first
+        long_text = "a" * 500
+        search_input.fill(long_text)
+        assert len(search_input.input_value()) > 0
+    
+    @pytest.mark.regression
+    def test_search_special_characters(self, page: Page, base_url: str):
+        home_page = HomePage(page)
+        home_page.open(base_url)
+        
+        search_input = page.locator('input[aria-label*="Meklēšanas ievadlauks"]').first
+        search_input.fill("@#$%")
+        assert search_input.input_value()
 
