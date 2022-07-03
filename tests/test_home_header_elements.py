a8d89e18bdf16e6423242c9928602e1ac637f2b4
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-07-03 13:54:10 -0500

    tests(home): add header elements tests (10 cases)

---
+++ tests/test_home_header_elements.py
@@ -0,0 +1,56 @@
+import pytest
+from playwright.sync_api import Page, expect
+from pages.home_page import HomePage
+
+
+class TestHomeHeaderElements:
+    
+    @pytest.mark.regression
+    def test_logo_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.logo).first).to_be_visible()
+
+    @pytest.mark.regression
+    def test_search_input_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.search_input).first).to_be_visible()
+
+    @pytest.mark.regression
+    def test_login_link_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.login_link).first).to_be_visible()
+
+    @pytest.mark.ui
+    def test_language_switcher_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.language_switcher).first).to_be_visible()
+
+    @pytest.mark.ui
+    def test_services_menu_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.services_menu).first).to_be_visible()
+
+    @pytest.mark.ui
+    def test_life_situations_menu_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.life_situations_menu).first).to_be_visible()
+
+    @pytest.mark.ui
+    def test_my_latvija_menu_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.my_latvija_menu).first).to_be_visible()
+
+    @pytest.mark.ui
+    def test_e_address_menu_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.e_address_menu).first).to_be_visible()
+
+    @pytest.mark.ui
+    def test_about_portal_menu_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.about_portal_menu).first).to_be_visible()
+
+    @pytest.mark.regression
+    def test_all_services_link_visible(self, page: Page, base_url: str):
+        home = HomePage(page).open(base_url).accept_cookies()
+        expect(page.locator(home.all_services_link).first).to_be_visible()
