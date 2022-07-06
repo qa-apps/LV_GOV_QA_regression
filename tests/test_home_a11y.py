435826cf8eccc1ebc70901131f1bc62cd4ac032c
Pavels Smoljaninovs <40374796+qa-apps@users.noreply.github.com>
2022-07-06 10:11:22 -0500

    tests: add 5 cases (batch 11)

--- tests/test_home_a11y.py
+++ tests/test_home_a11y.py
@@ -95,3 +95,38 @@ def test_auto_1_e(page: Page, base_url: str):
     hp.open(base_url)
     hp.accept_cookies()
     expect(page.locator(hp.all_services_link).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_11_a(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.logo).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_11_b(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.search_input).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_11_c(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.login_link).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_11_d(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.services_menu).first).to_be_visible()
+
+@pytest.mark.ui
+def test_auto_11_e(page: Page, base_url: str):
+    hp = HomePage(page)
+    hp.open(base_url)
+    hp.accept_cookies()
+    expect(page.locator(hp.all_services_link).first).to_be_visible()
